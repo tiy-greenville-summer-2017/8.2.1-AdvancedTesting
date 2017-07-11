7f8f2d6e4181598a6e2940465de4373ebda81cbe
taddeimania <jtaddei@gmail.com>
2017-07-11 10:42:11 -0400

    Added mongo stuffs

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
+
+const nodeEnv = process.env.NODE_ENV || "development";
+const config = require("./config.json")[nodeEnv];
+
+mongoose.connect(config.mongoURL);
 
 app.get("/api/sanity", (req, res) => {
   res.json({hello: "joel"});
